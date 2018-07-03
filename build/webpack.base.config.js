a35c484c6382014d6c50fa08bbbc725fa3cab71d
tingrun919 <344197917@qq.com>
2018-07-03 15:26:51 +0800

    修改build

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -14,7 +14,7 @@ module.exports = {
 		'vender-exten': '@/vendors/vendors.exten.js'
 	},
 	output: {
-		path: path.resolve(__dirname, '../dist')
+		path: path.resolve(__dirname, '../dist/dist')
 	},
 	devServer: {
 		historyApiFallback: {
