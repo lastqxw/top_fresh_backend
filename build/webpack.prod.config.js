f19a4dfcd0d2ecaa37a2491c0ae432b1afc584eb
tingrun919 <344197917@qq.com>
2018-07-03 15:17:22 +0800

    修改打包文件

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,7 +18,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'http://39.107.126.201/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名 
+        publicPath: '/admin',  // 修改 https://iv...admin 这部分为你的服务器域名 
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
@@ -74,7 +74,7 @@ module.exports = merge(webpackBaseConfig, {
         new HtmlWebpackPlugin({
             title: 'iView admin v' + package.version,
             favicon: './td_icon.ico',
-            filename: '../index.html',
+            filename: './index.html',
             template: '!!ejs-loader!./src/template/index.ejs',
             inject: false
         })
