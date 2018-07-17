a1b2df36ab6db7b612b3d5ec1cf5d2b65b41f201
tingrun919 <344197917@qq.com>
2018-07-17 18:39:56 +0800

    修改打包文件

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,7 +18,7 @@ fs.open('./build/env.js', 'w', function (err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: '/admin/', // 修改 https://iv...admin 这部分为你的服务器域名 
+        publicPath: 'http://39.107.126.201:82/dist/', // 修改 https://iv...admin 这部分为你的服务器域名 
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
